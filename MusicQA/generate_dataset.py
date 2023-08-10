c961784c3f9a0b4840d7cca606945781068016dd
Atin Sakkeer Hussain <atinsaki@gmail.com>
2023-08-10 10:55:21 +0800

    Dataset generation to wav files

--- MusicQA/generate_dataset.py
+++ MusicQA/generate_dataset.py
@@ -4,6 +4,8 @@ import os
 import argparse
 from tqdm import tqdm
 import json
+import torchaudio
+from pydub import AudioSegment
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--mtt', help='Directory of the MTT dataset', required=True)
@@ -21,34 +23,56 @@ if not os.path.exists(f"{args.musicqa}/audios"):
 
 musicqa = []
 
-q_set = ["Describe the audio", "Describe the audio in detail", "What do you hear in the audio?", "What can be inferred from the audio?"]
+q_set = ["Describe the audio", "Describe the audio in detail", "What do you hear in the audio?",
+         "What can be inferred from the audio?"]
 
 count = 0
 for i, row in tqdm(musiccaps_df.iterrows(), total=len(musiccaps_df)):
-    filename = f"{str(count).zfill(6)}.{row[0].split('.')[-1]}"
-    shutil.copy(f"{args.musiccaps}/audios/{row[0]}", f"{args.musicqa}/audios/{filename}")
-    for i in range(4):
-        musicqa.append({"audio_name": filename, 
-                        "conversation": [{"from": "human", "value": q_set[i]}, 
-                                         {"from": "gpt", "value": row[i+1]}]})
-    for qa in row[5:]:
-        musicqa.append({"audio_name": filename, 
-                        "conversation": [{"from": "human", "value": qa.split("\t")[0].replace("Q:", "")}, 
-                                         {"from": "gpt", "value": qa.split("\t")[1].replace("A:", "")}]})
-    count += 1
-    
+    if not os.path.exists(f"{args.musiccaps}/audios/{row[0]}"):
+        continue
+    try:
+        file_format = row[0].split('.')[-1]
+        filename = f"{str(count).zfill(6)}.wav"
+        if file_format == "mp3":
+            sound = AudioSegment.from_mp3(f"{args.musiccaps}/audios/{row[0]}")
+            sound.export(f"{args.musicqa}/audios/{filename}", format="wav")
+        else:
+            shutil.copy(f"{args.musiccaps}/audios/{row[0]}", f"{args.musicqa}/audios/{filename}")
+        _, _ = torchaudio.load(f"{args.musicqa}/audios/{filename}")
+        for i in range(4):
+            musicqa.append({"audio_name": filename,
+                            "conversation": [{"from": "human", "value": q_set[i]},
+                                             {"from": "gpt", "value": row[i + 1]}]})
+        for qa in row[5:]:
+            musicqa.append({"audio_name": filename,
+                            "conversation": [{"from": "human", "value": qa.split("\t")[0].replace("Q:", "")},
+                                             {"from": "gpt", "value": qa.split("\t")[1].replace("A:", "")}]})
+        count += 1
+    except:
+        print(f"Skipping {args.musiccaps}/audios/{row[0]}")
 
 for i, row in tqdm(mtt_df.iterrows(), total=len(mtt_df)):
-    filename = f"{str(count).zfill(6)}.{row[0].split('.')[-1]}"
-    shutil.copy(f"{args.mtt}/audios/{row[0]}", f"{args.musicqa}/audios/{filename}")
-    for i in range(4):
-        musicqa.append({"audio_name": filename, 
-                        "conversation": [{"from": "human", "value": q_set[i]}, 
-                                         {"from": "gpt", "value": row[i+1]}]})
-    for qa in row[5:]:
-        musicqa.append({"audio_name": filename, 
-                        "conversation": [{"from": "human", "value": qa.split("\t")[0].replace("Q:", "")}, 
-                                         {"from": "gpt", "value": qa.split("\t")[1].replace("A:", "")}]})
-    count += 1
+    if not os.path.exists(f"{args.mtt}/audios/{row[0]}"):
+        continue
+    try:
+        file_format = row[0].split('.')[-1]
+        filename = f"{str(count).zfill(6)}.wav"
+        if file_format == "mp3":
+            sound = AudioSegment.from_mp3(f"{args.mtt}/audios/{row[0]}")
+            sound.export(f"{args.musicqa}/audios/{filename}", format="wav")
+        else:
+            shutil.copy(f"{args.mtt}/audios/{row[0]}", f"{args.musicqa}/audios/{filename}")
+        _, _ = torchaudio.load(f"{args.musicqa}/audios/{filename}")
+        for i in range(4):
+            musicqa.append({"audio_name": filename,
+                            "conversation": [{"from": "human", "value": q_set[i]},
+                                             {"from": "gpt", "value": row[i + 1]}]})
+        for qa in row[5:]:
+            musicqa.append({"audio_name": filename,
+                            "conversation": [{"from": "human", "value": qa.split("\t")[0].replace("Q:", "")},
+                                             {"from": "gpt", "value": qa.split("\t")[1].replace("A:", "")}]})
+        count += 1
+    except:
+        print(f"Skipping {args.mtt}/audios/{row[0]}")
 
 json.dump(musicqa, open(f"{args.musicqa}/MusicQA.json", "w"), indent=2)
