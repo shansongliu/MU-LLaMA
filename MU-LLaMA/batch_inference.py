da7aeebffb2c218e571e60ee19408883c1b9a528
Atin Sakkeer Hussain <atinsaki@gmail.com>
2023-09-28 04:28:40 +0800

    Add Batch Inference Script

---
+++ MU-LLaMA/batch_inference.py
@@ -0,0 +1,82 @@
+import argparse
+import torch.cuda
+
+import llama
+from util.misc import *
+from data.utils import load_and_transform_audio_data
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--model", default="./ckpts/checkpoint.pth", type=str,
+    help="Name of or path to the trained checkpoint",
+)
+parser.add_argument(
+    "--llama_type", default="7B", type=str,
+    help="Type of llama original weight",
+)
+parser.add_argument(
+    "--llama_dir", default="/path/to/llama", type=str,
+    help="Path to LLaMA pretrained checkpoint",
+)
+parser.add_argument(
+    "--mert_path", default="m-a-p/MERT-v1-330M", type=str,
+    help="Path to MERT pretrained checkpoint",
+)
+parser.add_argument(
+    "--knn_dir", default="./ckpts", type=str,
+    help="Path to directory with KNN Index",
+)
+
+parser.add_argument(
+    "--audio_paths", nargs='+', required=True, type=str,
+    help="Paths to the input music file",
+)
+parser.add_argument(
+    "--questions", nargs='+', default="Describe the Audio", type=str,
+    help="Questions to ask the model",
+)
+
+args = parser.parse_args()
+model = llama.load(args.model, args.llama_dir, mert_path=args.mert_path, knn=True, knn_dir=args.knn_dir,
+                   llama_type=args.llama_type)
+model.eval()
+
+if len(args.questions) != len(args.audio_paths):
+    if len(args.questions) == 1:
+        args.questions = args.questions * len(args.audio_paths)
+    elif len(args.audio_paths) == 1:
+        args.audio_paths = args.audio_paths * len(args.questions)
+    else:
+        print("Number of Questions must match Number of Music files")
+        exit(0)
+
+
+def multimodal_generate(
+        audio_paths,
+        audio_weight,
+        prompts,
+        cache_size,
+        cache_t,
+        cache_weight,
+        max_gen_len,
+        gen_t, top_p
+):
+    inputs = {}
+    audio = load_and_transform_audio_data(audio_paths)
+    inputs['Audio'] = [audio, audio_weight]
+    prompts = [llama.format_prompt(prompt) for prompt in prompts]
+    prompts = [model.tokenizer.encode(x, bos=True, eos=False) for x in prompts]
+    with torch.cuda.amp.autocast():
+        results = model.generate(inputs, prompts, max_gen_len=max_gen_len, temperature=gen_t, top_p=top_p,
+                                 cache_size=cache_size, cache_t=cache_t, cache_weight=cache_weight)
+    text_outputs = [result.strip() for result in results]
+    return text_outputs
+
+
+outputs = multimodal_generate(args.audio_paths, 1, args.questions, 100, 20.0, 0.0, 512, 0.6, 0.8)
+
+for i in range(len(args.audio_paths)):
+    print()
+    print(f"Audio File: {args.audio_path[i]}")
+    print(f"Q: {args.questions[i]}")
+    print(f"A: {outputs[i]}")
